ddaa70f571e3939ee9257643f687e513b451da80
Lucas Ceratto <lucasceratto@gmail.com>
2023-09-09 18:02:41 -0300

    MON-20 parametrizar los valores de padding de prime react

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
-import "./styles/themes/theme.scss"                      //estilos tematicos
 import "primereact/resources/primereact.min.css"; //estilos core
 import 'primeicons/primeicons.css';               //estilos de iconos
+import "./styles/themes/theme.scss"                      //estilos tematicos
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
