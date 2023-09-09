daff33ff014bde0d201ef34595642f009c67e5f5
Lucas Ceratto <lucasceratto@gmail.com>
2023-09-09 13:53:27 -0300

    MON-18 Aplico el theme en sass de Prime React

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,14 @@
-import "./styles/index.scss"                      //estilos tematicos
+import "./styles/themes/theme.scss"                      //estilos tematicos
 import "primereact/resources/primereact.min.css"; //estilos core
 import 'primeicons/primeicons.css';               //estilos de iconos
 import ReactDOM from 'react-dom/client'
 import App from './App'
+
+//fonts
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
