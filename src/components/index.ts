7fbd0c07ac17b0b9bc92bb5763dc6bea56a46909
Lucas Ceratto <lucasceratto@gmail.com>
2023-09-10 17:48:55 -0300

    Reorganizo componentes

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./backgrounds/AuthBackground/AuthBackground"
-export * from "./PageContainer/Breadcrumbs/Breadcrumbs"
-export * from "./PageContainer/PageContainer"
+export * from "./shared/Breadcrumbs/Breadcrumbs"
+export * from "./shared/PageContainer/PageContainer"
+export * from "./shared/PageHeader/PageHeader"
