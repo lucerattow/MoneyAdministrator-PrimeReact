8de90e84490f878078fe2502eb06880d5b7d2243
Lucas Ceratto <lucasceratto@gmail.com>
2023-09-10 23:16:51 -0300

    MON-18 Muevo los estilos de App al index

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
-import PrimeReact from 'primereact/api';
-import { PrimeReactProvider } from 'primereact/api';
 import { AppContextProvider } from '@/context';
 import { AppRoutes } from '@/routes';
 
 const App = () => {
-  PrimeReact.ripple = true
-
   return (
-    <PrimeReactProvider>
-      <AppContextProvider>
-        <AppRoutes />
-      </AppContextProvider>
-    </PrimeReactProvider>
+    <AppContextProvider>
+      <AppRoutes />
+    </AppContextProvider>
   )
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,8 @@
+import "./styles/themes/theme.scss"               //estilos tematicos
 import "primereact/resources/primereact.min.css"; //estilos core
 import 'primeicons/primeicons.css';               //estilos de iconos
-import "./styles/themes/theme.scss"                      //estilos tematicos
+import PrimeReact from 'primereact/api';
+import { PrimeReactProvider } from 'primereact/api';
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
@@ -10,7 +12,12 @@ import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+PrimeReact.ripple = true
+
 ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
+).render(
+  <PrimeReactProvider>
+    <App />
+  </PrimeReactProvider>
 )
-.render(<App />)
