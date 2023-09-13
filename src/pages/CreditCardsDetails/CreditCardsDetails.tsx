57d78834d3007e3a349d364a4f178149dd1a2484
Lucas Ceratto <lucasceratto@gmail.com>
2023-09-13 18:53:47 -0300

    MON-36 Desarrollo de botones "Crear" e "Importar"

--- src/pages/CreditCardsDetails/CreditCardsDetails.tsx
+++ src/pages/CreditCardsDetails/CreditCardsDetails.tsx
@@ -1,7 +1,14 @@
 import React from "react"
 import classNames from "classnames"
 import { useAppContext } from '@/hooks';
-import { PageHeader, CreditCard, CreditCardDetails } from "@/components"
+import { Button } from 'primereact/button';
+import UploadFileIcon from '@mui/icons-material/UploadFile';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+import {
+  PageHeader,
+  CreditCard,
+  CreditCardDetails,
+} from "@/components"
 import styles from "./CreditCardsDetails.module.scss"
 
 export type CreditCardsDetailsProps = {
@@ -19,12 +26,30 @@ export const CreditCardsDetails: React.FC<CreditCardsDetailsProps> = ({ classNam
     ]);
   }, [])
 
+  const handleImportClick = () => console.log("Importar PDF")
+
+  const handleCreateClick = () => console.log("Crear resumen")
+
   return (
     <div className={classNames(className)}>
       <PageHeader title={title} buttonBack />
       <div className={styles.container}>
         <div className={styles.column_left}>
           <CreditCard />
+          <span className={classNames("p-buttonset", styles.buttons)}>
+            <Button
+              className={styles.button}
+              icon={() => <UploadFileIcon />}
+              label="Importar"
+              onClick={handleImportClick}
+            />
+            <Button
+              className={styles.button}
+              icon={() => <AddCircleOutlineOutlinedIcon />}
+              label="Crear"
+              onClick={handleCreateClick}
+            />
+          </span>
         </div>
         <div className={styles.column_right}>
           <CreditCardDetails />
