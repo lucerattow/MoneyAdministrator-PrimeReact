bd2cc9c368b0b331abccde3e20520913677c185c
Lucas Ceratto <lucasceratto@gmail.com>
2023-09-19 23:28:13 -0300

    MON-49 Continuo desarrollo del inputSelect

--- src/components/controls/InputSelect/InputSelect.tsx
+++ src/components/controls/InputSelect/InputSelect.tsx
@@ -6,28 +6,30 @@ import styles from "./InputSelect.module.scss"
 
 export type InputSelectProps<T extends FieldValues> = {
   className?: string
+  inputClassName?: string
   name: keyof T
   label: string
   control: Control<T>
   errors: FieldErrors<T>
+  options?: any[]
+  optionLabel: string
   defaultValue?: string
   autoFocus?: boolean
   autoComplete?: string
-  leftIcon?: React.ReactNode
-  rightIcon?: React.ReactNode
 }
 
 export const InputSelect: React.FC<InputSelectProps<any>> = ({
   className,
+  inputClassName,
   name,
   label,
   control,
   errors,
+  options,
+  optionLabel,
   defaultValue = "",
   autoFocus = false,
   autoComplete = "",
-  leftIcon,
-  rightIcon,
 }) => {
   const errorMessage = errors[name as string]?.message as string
   const _name = name as string
@@ -42,26 +44,25 @@ export const InputSelect: React.FC<InputSelectProps<any>> = ({
       control={control}
       defaultValue={defaultValue}
       render={({ field }) => (
-        <div className={classNames(
-          className,
-          styles.container
-        )}>
+        <div className={classNames(className, styles.container)}>
           <label htmlFor={_name}>{label}</label>
           <span className={classNames(styles.input)}>
-            {leftIcon}
             <Dropdown
               {...field}
               className={classNames(
+                inputClassName,
                 styles.input,
                 isFormFieldInvalid() && "p-invalid"
               )}
               type="text"
               id={_name}
               name={_name}
+              options={options}
+              optionLabel={optionLabel}
               autoComplete={autoComplete}
               autoFocus={autoFocus}
+              onChange={(e) => field.onChange(e.value)}
             />
-            {rightIcon}
           </span>
           {FormErrorMessage()}
         </div>
